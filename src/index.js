91e1a8e8eeb44e0d1d77ce81a94ddbf893a00c1c
José Perigolo Neto <51827621+Jose-Perigolo@users.noreply.github.com>
2022-02-21 18:25:51 -0300

    Challenge Completed
    Passed on all tests

--- src/index.js
+++ src/index.js
@@ -11,18 +11,76 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({error: 'User not found'});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  const { user } = request
+
+  const userTodosAvailability = (user.todos.length < 10) || (user.pro)
+
+  if (!userTodosAvailability) {
+    return response.status(403).json({error: 'Please subscribe to PRO to create more tasks'})
+  }
+
+  return next();
+
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({error: 'User not found'})
+  }
+
+  const isUuid =  validate(id)
+
+  if (!isUuid) {
+    return response.status(400).json({error: 'Invalid ID'})
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if (!todo) {
+    return response.status(404).json({error: 'Task not found'})
+  }
+
+  request.user = user
+  request.todo = todo
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
   // Complete aqui
+  const { id } = request.params
+
+  const user = users.find(user => user.id === id)
+
+  if (!user) {
+    return response.status(404).json({error: 'User not found'})
+  }
+
+  request.user = user
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
